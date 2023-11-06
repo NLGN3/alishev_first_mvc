8e589acb12728fbe0f65938df9cdcafe8db360f4
NLGN3 <a1945230@yandex.ru>
2023-11-06 22:32:44 +0300

    начали работу с параметрами

--- src/main/java/ru/alishev/springcourse/controllers/firstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/firstController.java
@@ -1,8 +1,10 @@
 package ru.alishev.springcourse.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 //теперь до всех методов надо идти через указанный путь. Например /first/hello
@@ -10,8 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/first")
 public class firstController {
     @GetMapping("/hello")
-    public String helloPage() { return "first/hello"; } //здесь first - это навзание папки, не путать с first в маппинге класса!
+    //аргумент HttpServletRequest, если мы его указали, будет внедрен спрингом автоматически. Он содержит параметры запроса от браузера (
+    public String helloPage(HttpServletRequest resuest) {
+        String name = resuest.getParameter("name");
+        String surname = resuest.getParameter("surname");
+
+        System.out.println("Hello " + name + " " + surname);
+        return "first/hello";
+    } //здесь first - это название папки, не путать с first в маппинге класса!
 
     @GetMapping("/goodbye")
-    public String goodbyePage() { return "first/goodbye"; }
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello " + name + " " + surname);
+        return "first/goodbye";
+    }
 }
